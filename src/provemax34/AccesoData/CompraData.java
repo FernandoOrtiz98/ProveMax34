738682e1ed16a7550254856ca2116ae94e708d60
Izack2000 <leon.cba@gmail.com>
2023-10-03 21:57:30 -0300

    Update CompraData.java

--- src/provemax34/AccesoData/CompraData.java
+++ src/provemax34/AccesoData/CompraData.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package provemax34.AccesoData;
 
-/**
- *
- * @author Fer
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import provemax34.Entidades.Compra;
+
+
 public class CompraData {
+    
+    private Connection con=null;
+    private ProveedorData proveedorD= new ProveedorData();
+    
+
+    public CompraData() {
+        this.con = Conexion.getConexion();
+    }
+    
+    public void guardarCompra (Compra compra, int idProveedor){                
+        
+        String query="INSERT INTO compra(idProveedor,fecha) VALUES (?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, compra.getidProveedor());               
+                ps.setDate(2, Date.valueOf(compra.getfecha()));
+                
+                ps.executeUpdate();
+                
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                alum.setIdAlumno(rs.getInt(1));
+            }else{
+                JOptionPane.showMessageDialog(null,"No se pudo tener el ID...");
+            }
+            ps.close();
+            JOptionPane.showMessageDialog(null,"Guardado!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error de conexion... "+ex.getMessage());
+        }
+    }
+    
     
 }
