1607548ba492ea377eeca32a6883cd2d81c65de5
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-10-05 21:32:56 -0300

    Update AgregarProducto.java

--- src/provemax34/Vistas/AgregarProducto.java
+++ src/provemax34/Vistas/AgregarProducto.java
@@ -168,7 +168,7 @@ public static void main(String args[]) {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new AgregarProducto().setVisible(true);
             }
         });
     }
