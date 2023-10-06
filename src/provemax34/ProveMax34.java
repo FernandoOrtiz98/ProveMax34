81d50ce8a711022d81f27940bf22ae8404fe1cba
JosCapdev <josecapdevila512@gmail.com>
2023-10-06 18:35:57 -0300

    Update ProveMax34.java

--- src/provemax34/ProveMax34.java
+++ src/provemax34/ProveMax34.java
@@ -1,12 +1,15 @@
 
 package provemax34;
 
+import provemax34.AccesoData.ProductoData;
+
 
 public class ProveMax34 {
 
     
     public static void main(String[] args) {
-        
+        ProductoData prodData=new ProductoData(); 
+        System.out.println(prodData.listarProductos());
     }
     
 }
