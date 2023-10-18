49e893cc0c8e9dbf36d834041cedf4ebc6d35b5e
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-10-18 18:26:59 -0300

    Update ProveMax34.java

--- src/provemax34/ProveMax34.java
+++ src/provemax34/ProveMax34.java
@@ -2,6 +2,7 @@
 package provemax34;
 
 import provemax34.AccesoData.ProductoData;
+import provemax34.AccesoData.ProveedorData;
 import provemax34.Entidades.Producto;
 
 
@@ -9,13 +10,16 @@ public class ProveMax34 {
 
     
     public static void main(String[] args) {
-        ProductoData prodD=new ProductoData();
+//        ProductoData prodD=new ProductoData();
 //        Producto prod=new Producto("Heladera","Electrolux catA",150000, 5, true);
 //        
-       Producto prod= new Producto(1,"Heladera","Patrick catA",180000, 5, true);
+//       Producto prod= new Producto(1,"Heladera","Patrick catA",180000, 5, true);
 //         prodD.guardarProducto(prod);
 //        prodD.modificarProducto(prod);
 //        prodD.eliminarProducto(prod.getIdProducto());
+          ProveedorData provD= new ProveedorData();
+          String result= provD.buscarProveedor(1)+"";
+          System.out.println(result);
     }
     
 }
