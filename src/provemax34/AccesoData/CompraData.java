ae76455426904509075526fcd318e3c4b3ca8138
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-10-18 18:26:49 -0300

    Update CompraData.java

--- src/provemax34/AccesoData/CompraData.java
+++ src/provemax34/AccesoData/CompraData.java
@@ -19,7 +19,7 @@ public class CompraData {
     private Connection con = null;
     private Proveedor proveedor = new Proveedor();
     private Compra compra = null;
-    ProveedorData proveedorD = new ProveedorData();
+    private ProveedorData proveedorD = new ProveedorData();
 
     public CompraData() {
         this.con = Conexion.getConexion();
@@ -87,63 +87,34 @@ public class CompraData {
             
         }
     }
-    
-    
-    
-    
-    
-    
 
-//    public List<Compra> listarComprasPorProveedor(int idProveedor) {
-//
-//        List<Compra> compras = new ArrayList<>();                 //int idCompra   Proveedor proveedor   LocalDate fecha
-//        String sql = "SELECT * FROM compra WHERE idProveedor = ?";
-//        try {
-//            PreparedStatement ps = con.prepareStatement(sql);
-//            ps.setInt(1, idProveedor);
-//            ResultSet rs = ps.executeQuery();
-//            //System.out.println(" dsadsa"+idAlumno);
-//            while (rs.next()) {
-//                //Compra compra = new Compra();
-//                compra.setIdCompra(rs.getInt("idCompra"));
-//                Proveedor proveedor = proveedorD.buscarProveedor(rs.getInt("idProveedor"));
-//                compra.setFecha(rs.getDate("fecha").toLocalDate());
-//                
-//                compras.add(compra);
-//                //System.out.println("esto se va guardar en la lista inscripciones"+insc);
-//
-//            }
-//            ps.close();
-//        } catch (SQLException ex) {
-//            JOptionPane.showMessageDialog(null, "Error de conexion..." + ex.getMessage());
-//        }
-//
-//        return compras;
-//
-//    }
-    
-    
-    /*public void guardarCompra (int idProveedor, LocalDate fecha){                
-    
-        String query="INSERT INTO compra(idProveedor,fecha) VALUES (?,?)";
+    public List<Compra> listarComprasPorProveedor(int idProveedor) {
+
+        List<Compra> compras = new ArrayList<>();                 //int idCompra   Proveedor proveedor   LocalDate fecha
+        String sql = "SELECT * FROM compra WHERE idProveedor = ?";
         try {
-            PreparedStatement ps = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
-                ps.setInt(1,idProveedor );               //compra.getProveedor().getIdProveedor()
-                ps.setDate(2, Date.valueOf(fecha));  //compra.getFecha()
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idProveedor);
+            ResultSet rs = ps.executeQuery();
+            //System.out.println(" dsadsa"+idAlumno);
+            while (rs.next()) {
+                //Compra compra = new Compra();
+                compra.setIdCompra(rs.getInt("idCompra"));
+                Proveedor proveedor = proveedorD.buscarProveedor(rs.getInt("idProveedor"));
+                compra.setFecha(rs.getDate("fecha").toLocalDate());
                 
-                ps.executeUpdate();
-                
-            ResultSet rs = ps.getGeneratedKeys();
-            if(rs.next()){
-                compra.setIdCompra(rs.getInt(1));
-            }else{
-                JOptionPane.showMessageDialog(null,"No se pudo tener el ID...");
+                compras.add(compra);
+                //System.out.println("esto se va guardar en la lista inscripciones"+insc);
+
             }
             ps.close();
-            JOptionPane.showMessageDialog(null,"Guardado!");
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null,"Error de conexion... "+ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error de conexion..." + ex.getMessage());
         }
-    }*/
 
+        return compras;
+
+    }
+    
+    
 }
