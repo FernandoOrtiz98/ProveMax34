d09587b6203c0d539f79b54a30319205bd47cbdf
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-10-29 21:47:21 -0300

    Update provemax.sql

--- provemax.sql
+++ provemax.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-10-2023 a las 02:16:26
+-- Tiempo de generación: 30-10-2023 a las 01:46:06
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -31,9 +31,20 @@ USE `provemax`;
 
 CREATE TABLE `compra` (
   `idCompra` int(11) NOT NULL,
-  `proveedor` int(11) NOT NULL,
+  `idProveedor` int(11) NOT NULL,
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `compra`
+--
+
+INSERT INTO `compra` (`idCompra`, `idProveedor`, `fecha`) VALUES
+(8, 4, '2023-10-10'),
+(9, 2, '2023-10-13'),
+(10, 1, '2023-10-02'),
+(11, 5, '2023-10-05'),
+(12, 3, '2023-10-07');
 
 -- --------------------------------------------------------
 
@@ -45,9 +56,22 @@ CREATE TABLE `detallecompra` (
   `idDetalle` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `precioCosto` double NOT NULL,
-  `compra` int(11) NOT NULL,
-  `producto` int(11) NOT NULL
+  `idCompra` int(11) NOT NULL,
+  `idProducto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `detallecompra`
+--
+
+INSERT INTO `detallecompra` (`idDetalle`, `cantidad`, `precioCosto`, `idCompra`, `idProducto`) VALUES
+(2, 3, 90000, 8, 3),
+(3, 3, 70000, 8, 5),
+(4, 3, 100000, 9, 2),
+(5, 3, 120000, 9, 3),
+(6, 0, 1234, 10, 1),
+(7, 10, 12345, 10, 4),
+(8, 0, 123456, 11, 6);
 
 -- --------------------------------------------------------
 
@@ -64,6 +88,19 @@ CREATE TABLE `producto` (
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`idProducto`, `nombreProducto`, `descripcion`, `precioActual`, `stock`, `estado`) VALUES
+(1, 'Heladera', 'Patrick catA', 180000, 5, 1),
+(2, 'SmartTv', '43 pulgadas', 140000, 10, 1),
+(3, 'celular', 'a32, 4gb ram, 64gb', 170000, 6, 1),
+(4, 'Lavarropas', 'Whirlpool', 180000, 5, 1),
+(5, 'Microondas', 'Atma', 80000, 7, 1),
+(6, 'AireAcondicionado', 'split', 90000, 6, 1),
+(7, 'asdsdsd', 'adsadsa', 123, 12, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -74,15 +111,27 @@ CREATE TABLE `proveedor` (
   `idProveedor` int(11) NOT NULL,
   `razonSocial` char(60) NOT NULL,
   `domicilio` char(60) NOT NULL,
-  `telefono` char(60) NOT NULL
+  `telefono` char(60) NOT NULL,
+  `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `proveedor`
 --
 
-INSERT INTO `proveedor` (`idProveedor`, `razonSocial`, `domicilio`, `telefono`) VALUES
-(1, 'Marolio', 'domicilioprueba 1', '1234567');
+INSERT INTO `proveedor` (`idProveedor`, `razonSocial`, `domicilio`, `telefono`, `estado`) VALUES
+(1, 'Noblex', 'domicilioprueba 1', '1234567', 1),
+(2, 'Samsung', 'domicilio1', '123123', 1),
+(3, 'Motorola', 'chacabuco 123', '12345', 1),
+(4, 'LG', 'Pedernera 512', '304050', 1),
+(5, 'Dream', 'Mitre 1040', '232425', 1),
+(6, 'Patrick', 'necochea 220', '432142', 1),
+(7, 'Coinor', 'Ardiles 1540', '424242', 1),
+(8, 'xiaomi', 'Tallaferro 878', '123412', 1),
+(9, 'asdas', 'adsadas', '123123', 0),
+(10, 'adsadas', 'adsad', '123123', 0),
+(11, 'adsadsa', 'asdsadsada', 'asdsadsada', 0),
+(12, 'adsadsa', 'asdsad', '1234', 0);
 
 --
 -- Índices para tablas volcadas
@@ -93,15 +142,15 @@ INSERT INTO `proveedor` (`idProveedor`, `razonSocial`, `domicilio`, `telefono`) 
 --
 ALTER TABLE `compra`
   ADD PRIMARY KEY (`idCompra`),
-  ADD KEY `proveedor` (`proveedor`);
+  ADD KEY `proveedor` (`idProveedor`);
 
 --
 -- Indices de la tabla `detallecompra`
 --
 ALTER TABLE `detallecompra`
   ADD PRIMARY KEY (`idDetalle`),
-  ADD KEY `compra` (`compra`),
-  ADD KEY `producto` (`producto`);
+  ADD KEY `compra` (`idCompra`),
+  ADD KEY `producto` (`idProducto`);
 
 --
 -- Indices de la tabla `producto`
@@ -123,25 +172,25 @@ ALTER TABLE `proveedor`
 -- AUTO_INCREMENT de la tabla `compra`
 --
 ALTER TABLE `compra`
-  MODIFY `idCompra` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCompra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `detallecompra`
 --
 ALTER TABLE `detallecompra`
-  MODIFY `idDetalle` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idDetalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedor`
 --
 ALTER TABLE `proveedor`
-  MODIFY `idProveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idProveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restricciones para tablas volcadas
@@ -151,14 +200,14 @@ ALTER TABLE `proveedor`
 -- Filtros para la tabla `compra`
 --
 ALTER TABLE `compra`
-  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`proveedor`) REFERENCES `proveedor` (`idProveedor`);
+  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`idProveedor`) REFERENCES `proveedor` (`idProveedor`);
 
 --
 -- Filtros para la tabla `detallecompra`
 --
 ALTER TABLE `detallecompra`
-  ADD CONSTRAINT `detallecompra_ibfk_1` FOREIGN KEY (`compra`) REFERENCES `compra` (`idCompra`),
-  ADD CONSTRAINT `detallecompra_ibfk_2` FOREIGN KEY (`producto`) REFERENCES `producto` (`idProducto`);
+  ADD CONSTRAINT `detallecompra_ibfk_1` FOREIGN KEY (`idCompra`) REFERENCES `compra` (`idCompra`),
+  ADD CONSTRAINT `detallecompra_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
